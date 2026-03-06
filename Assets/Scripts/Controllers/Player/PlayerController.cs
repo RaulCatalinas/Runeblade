601ade62b13f8e1fcd8ceec77d535bfcacdc9893
Raul Catalinas <raulcatalinas@gmail.com>
2026-03-06 13:40:15 +0100

    feat: save last activated checkpoint

--- Assets/Scripts/Controllers/Player/PlayerController.cs
+++ Assets/Scripts/Controllers/Player/PlayerController.cs
@@ -6,10 +6,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private PlayerMovementController movementController;
     [SerializeField] private PlayerCombatController combatController;
     [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Transform startPoint;
 
     private bool jumpRequested = false;
     private bool attackRequested = false;
     private bool dashRequested = false;
+    private Vector3 lastCheckpointActivated;
+
+    void Start()
+    {
+        lastCheckpointActivated = startPoint.position;
+        gameObject.transform.position = startPoint.position;
+    }
 
     void FixedUpdate()
     {
@@ -56,5 +64,23 @@ public class PlayerController : MonoBehaviour
     public void OnDash(InputValue value)
     {
         if (value.isPressed) dashRequested = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Checkpoint"))
+        {
+            Debug.Log("Checkpoint activated");
+            lastCheckpointActivated = collision.transform.position;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("WorldLimit"))
+        {
+            Debug.Log("The player has gone beyond the boundaries of the world. Reappearing in the last safe position.");
+            gameObject.transform.position = lastCheckpointActivated;
+        }
     }
 }
