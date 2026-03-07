51d4a78bb37bd96df38a50869ae381ec1c7adb91
Raul Catalinas <raulcatalinas@gmail.com>
2026-03-07 01:23:14 +0100

    fix: move player along with moving platform

--- Assets/Scripts/Controllers/WaypointPlatform/WaypointPlatformController.cs
+++ Assets/Scripts/Controllers/WaypointPlatform/WaypointPlatformController.cs
@@ -14,37 +14,45 @@ public class WaypointPlatformController : MonoBehaviour
     private bool goingForward = true;
     private RigidbodyConstraints2D horizontalConstraints;
     private RigidbodyConstraints2D verticalConstraints;
+    private Vector2 lastPosition;
+    private Vector2 platformVelocity;
+    private Rigidbody2D rbPlayer;
 
     void Awake()
     {
-        horizontalConstraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionY;
-        verticalConstraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX;
-    }
-
-    void Start()
-    {
         if (waypoints.Length < 2)
         {
-            throw new InvalidOperationException("At least 2 waypoints are required for the platform to move.");
+            throw new InvalidOperationException(
+                "At least 2 waypoints are required for the platform to move."
+            );
         }
+
+        horizontalConstraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionY;
+        verticalConstraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX;
+
+        lastPosition = rb.position;
     }
 
     void FixedUpdate()
     {
-
+        platformVelocity = (rb.position - lastPosition) / Time.fixedDeltaTime;
+        lastPosition = rb.position;
 
         if (!randomizeMovement) MoveOnOrder();
         else MoveRandomly();
+
+        if (rbPlayer != null)
+        {
+            rbPlayer.position += platformVelocity * Time.fixedDeltaTime;
+        }
     }
 
     void MoveOnOrder()
     {
         var target = waypoints[currentWaypointIndex].position;
-
-        FreezeRigidbody(target);
-
         var newPos = Vector2.MoveTowards(rb.position, target, moveSpeed * Time.fixedDeltaTime);
 
+        FreezeRigidbody(target);
         rb.MovePosition(newPos);
 
         if (Vector2.Distance(rb.position, target) > arrivalThreshold) return;
@@ -83,5 +91,19 @@ public class WaypointPlatformController : MonoBehaviour
         var moveHorizontally = Mathf.Abs(direction.x) > Mathf.Abs(direction.y);
 
         rb.constraints = moveHorizontally ? horizontalConstraints : verticalConstraints;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.collider.CompareTag("Player")) return;
+
+        rbPlayer = collision.collider.GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (!collision.collider.CompareTag("Player")) return;
+
+        rbPlayer = null;
     }
 }
